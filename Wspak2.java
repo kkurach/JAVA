3a7d80f7359f069002d8919f192f12f6e92c6c17
2KK <kamilkurach@yahoo.pl>
2015-08-22 22:01:06 +0200

    .

--- Wspak2.java
+++ Wspak2.java
@@ -1,4 +1,19 @@
 // zadanie 3.5
+
 public class Wspak2 {
-	
+	public static void main(String args[]){
+		
+		char dane[] = {'p','r','o','g','r','a','m','o','w','a','n','i','e'};
+		
+		
+		for(int j = dane.length - 1, i = 0; i < j; i++, j--){
+			
+			char p = dane[i];
+			dane[i] = dane[j];
+			dane[j] = p;
+		
+		
+		}
+		System.out.println(dane);
+	}
 }
