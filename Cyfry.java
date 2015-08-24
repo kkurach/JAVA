a55fe9f7ffd4da38dcfe6b6b84725aa1c753458a
2KK <kamilkurach@yahoo.pl>
2015-08-24 19:46:39 +0200

    new

---
+++ Cyfry.java
@@ -0,0 +1,18 @@
+// zadanie 3.7
+
+public class Cyfry {
+	public static void main(String args[]){
+		
+		
+		char dane[];
+		dane = new char[10];
+		for(int i = 0; i < dane.length; i++){
+			char b = (char) i;
+			dane.append(b);
+		}
+		
+		
+		System.out.println(b);	
+	}
+	
+}
