5d568bee50fa5ec2c045ab5474c5e077d6e959a0
KK <kkurach@users.noreply.github.com>
2015-08-25 22:22:47 +0200

    Fix style in Cyfry.java

--- Cyfry.java
+++ Cyfry.java
@@ -2,15 +2,10 @@
 
 public class Cyfry {
 	public static void main(String args[]){
-		
-		
-		char dane[];
-		dane = new char[10];
+		char dane[] = new char[10];
 		for(int i = 0; i < dane.length; i++){
 			
 		}
-		
-		
 		System.out.println(b);	
 	}
 	
